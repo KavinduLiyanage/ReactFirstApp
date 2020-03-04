4263576c533b44de1724f8f043eda384d82af5ea
kavinduliyanage <sandeepa8956@gmail.com>
2020-03-04 12:31:05 +0530

    ReactApp1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts:[]
+    }
+  }
+  componentDidMount() {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    fetch(url).then(response => response.json()).then(json => this.setState({posts:json}))
+  }
+  render() {
+    const { posts } = this.state;
+    return (
+        <div className="container">
+          <div className="jumbotron">
+            <h1 className="display-4">Blog posts</h1>
+          </div>
+          {posts.map((post) => (
+              <div className="card" key={post.id}>
+                <div className="card-header">
+                  #{post.id} {post.title}
+                </div>
+            <div className="card-body">
+              <p className="card-text">{post.body}</p>
+            </div>
+          </div>
+          ))}
+        </div>
+    )
+  }
+}
+
+export default App;
+/*
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world
         </p>
         <a
           className="App-link"
@@ -24,3 +59,4 @@ function App() {
 }
 
 export default App;
+*/
